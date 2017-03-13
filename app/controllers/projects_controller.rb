b33dc012de59da8aa2c7219ea1f3c8fa3d610426
StrannikAD <a.dimitrova.au@gmail.com>
2017-03-13 13:13:49 +1100

    Adding Project view functionality

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -2,4 +2,18 @@ class ProjectsController < ApplicationController
   def index
     @projects = Project.all.order("created_at desc")
   end
+
+  def new
+    @project = Project.new
+  end
+
+  def create
+    @project = Project.new project_params
+  end
+
+  private
+
+  def project_params
+    params.require(:project).permit(:title, :description, :link)
+  end
 end
